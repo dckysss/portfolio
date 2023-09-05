f340d5d32026fdd735c64fe26453626afce50dcc
Dicky Sanjaya <dickysanjaya003@gmail.com>
2023-09-05 14:04:12 +0700

    about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => (
     <section id="about-me" className={`${styles.paddingY} mt-6`}>
       <div className="flex-1 flex-col text-center mb-10">
         <h2 className={styles.heading2}>About Me</h2>
-        <p className={styles.paragraph}>Hello everyone, my name is Dicky Sanjaya, I'm a student in Binus University currently majoring Information Systems streaming Digital Technology. I have learned the basic of Java, UI/UX designing from the college, and now I'm learning and got interested in Front-end web developing. In this website, I'll put my web projects to show you guys what I've learned or achieved. You guys can also visit my github account by clicking the github icon in the hero or footer section. Thank you.</p>
+        <p className={styles.paragraph}>Hello everyone, my name is Dicky Sanjaya, I'm a student at Binus University currently majoring Information Systems. I have learned the basic of Java, UI/UX designing from the college, and now I'm learning and got interested in Front-end web developing. In this website, I put my web projects to show you guys what I've learned or achieved. You guys can also visit my github account by clicking the github icon in the hero or footer section. Thank you.</p>
       </div>
 
       <div className="flex-1 flex-col text-center">

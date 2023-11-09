3b1c9fab82f83fb654bbeee2ac7eb5a3c8795e51
Dicky Sanjaya <dickysanjaya003@gmail.com>
2023-11-09 14:32:20 +0700

    change to lazyload

--- src/components/SocMed.jsx
+++ src/components/SocMed.jsx
@@ -1,11 +1,10 @@
-import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { socialMedia } from '../constants'
 
 const SocMed = () => (
     <section className="flex flex-wrap xl:px-0 px-16 justify-center sm:justify-start">
       {socialMedia.map((social, index) => (
         <a href={social.link} target='_blank'>
-        <LazyLoadImage key={social.id} src={social.icon} alt={social.id} 
+        <img key={social.id} src={social.icon} alt={social.id} loading='lazy'
         className={`w-[48px] h-[48px] object-contain cursor-pointer ${index !== socialMedia.length -1 ? 'mr-10' : 'mr-0' }`} />
         </a>
       ))}
